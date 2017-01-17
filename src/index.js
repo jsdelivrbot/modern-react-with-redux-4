fbdd5ba6ad37294fcff1e122b2e278180b9335a4
reintrance <reintrance@gmail.com>
2017-01-17 08:34:14 +0300

    Session 23

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyArjHofWtKAGKRShRvofDrhZ2kity9EAe8';
 
@@ -19,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
